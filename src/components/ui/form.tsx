d9759306ce5f60a35b9ed28023e7ce1dc154f49b
bee-healthtech <byoyo183@gmail.com>
2026-04-22 18:21:55 -0300

    Fix form.tsx type syntax

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -18,7 +18,7 @@ import { Label } from "@/components/ui/label"
 
 const Form = FormProvider
 
-type FormFieldContextValue
+type FormFieldContextValue <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 > = {
